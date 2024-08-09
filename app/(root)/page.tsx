6b9cc88bf44cd2d4e3d5fb08de1eda4f3f8c994f
saifmalik2004 <saifmalik7827@gmail.com>
2024-08-09 16:40:13 +0530

    set up athentication  by clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+const  SetupPage= ()=> {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
+
+
+export default SetupPage;
